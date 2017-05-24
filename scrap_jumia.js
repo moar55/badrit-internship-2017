48abf8d7de0d5bc80e092b45fce12e9bfc903f14
moar55 <moar.ahmed@gmail.com>
2017-05-24 14:43:01 +0200

    checkpoint

--- scrap_jumia.js
+++ scrap_jumia.js
@@ -2,22 +2,59 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 const url = "https://www.jumia.com.eg/mobiles-tablets";
-request(url, function(error, response, html){
+const serverURL = "http://localhost:3000"
 
 
-        if(!error){
+request(url, function(error, response, html) {
 
-            var $ = cheerio.load(html);
 
-            // Finally, we'll define the variables we're going to capture
-            $(".products").filter(function () {
-              var data = $(this);
+    if (!error) {
 
-              data.
+        var $ = cheerio.load(html);
+
+        // Finally, we'll define the variables we're going to capture
+        $(".products").filter(function() {
+            var data = $(this);
+
+            data.find(".sku").each(function(index) {
+                var productLink = $(this).find('a').attr('href')
+                request(productLink, function(error, response, html) {
+                    var json = {
+                        name: "",
+                        desc: "",
+                        price: "40",
+                        created:Date.now(),
+                        user:'ObjectId("5911e0574b3074f1bcf19cfd")'
+                    };
+
+                    var $ = cheerio.load(html)
+                    var data = $(this)
+                    $(".details").filter(function() {
+                        json.name = $(this).find('h1').text();
+                        $(this).find('ul').each(function() {
+                            json.desc += $(this).find('li').text()
+                        });
+                    })
+                    request.post(
+                        serverURL+'/api/products',
+                            json
+                        ,
+                        function(error, response, body) {
+                          console.log(json);
+                            if (!error && response.statusCode == 200) {
+                                console.log(body)
+                            }
+                            else{
+                              console.log(response);
+                            }
+                        }
+                    );
+
+                })
             })
-            // var name, desc, price;
-            // var json = { name : "", desc : "", price : ""};
-        }
-    })
+
+        })
+    }
+})
