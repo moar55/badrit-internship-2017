2193e665134c64021b41566e7b143d6a60419a34
moar55 <moar.ahmed@gmail.com>
2017-05-24 14:08:25 +0200

    first

---
+++ scrap_jumia.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+const url = "https://www.jumia.com.eg/mobiles-tablets";
+request(url, function(error, response, html){
+
+
+        if(!error){
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+            $(".products").filter(function () {
+              var data = $(this);
+
+              console.log(data);
+            })
+            // var name, desc, price;
+            // var json = { name : "", desc : "", price : ""};
+        }
+    })
